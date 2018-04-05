79fad8ab8d9d857163e52611488594ba776b9d8d
Maximiliano Giuliani <maxgyu@gmail.com>
2018-04-05 16:00:00 -0300

    Exercise pointers

---
+++ pointers.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+void ejer_1();
+void ejer_2();
+void ejer_3();
+void ejer_4();
+void ejer_5();
+void ejer_6();
+void ejer_7();
+void ejer_8();
+
+int main()
+{
+    printf("Guia punteros");
+    return 0;
+}
+
+void ejer_1(){
+    //y = 5
+    //x = 4
+    //punt apunta a y
+}
+
+void ejer_2(){
+    //y = 3
+    //x = 9
+    //punt apunta a y
+}
+
+void ejer_3(){
+    //y = 3
+    //puntb apunta a y = 3
+    //x = 9
+}
+
+void ejer_4(){
+    //el array queda como [9,2,3,4,5]
+}
+
+void ejer_5(){}
+
+void ejer_6(){}
+
+void ejer_7(){
+ //1) La pos cabecera del array
+ //2) Correr la cabecera del array a la pos 2
+ //3) El contenido de la cabecera del array
+ //4) Al contenido de la cabecera le sumo 2
+ //5) el contenido del array en la posicion 3
+}
+
+void ejer_8(){
+ //1) A[4] = 34
+ //2) Muestra 21
+ //3) Muestra 27
+ //4) Muestra 34
+ //5) Muestra 34
+
+}
+
